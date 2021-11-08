59e3e8eb55ec9a71f9bf0c76ae27bba2c29e6597
SketchTurnerrr <nareknarek45@gmail.com>
2021-11-08 21:01:31 +0200

    :fire:

--- gh-pages.js
+++ gh-pages.js
@@ -3,12 +3,13 @@ var ghpages = require('gh-pages');
 ghpages.publish(
   'public', // path to public directory
   {
-    branch: 'gh-pages',
+    branch: 'main',
     repo: 'https://github.com/SketchTurnerrr/space-tourism-svelte', // Update to point to your repository
     user: {
-      name: 'Narek', // update to use your name
-      email: 'Email', // Update to use your email
+      name: 'Narek',
+      email: 'Email',
     },
+    dotfiles: true,
   },
   () => {
     console.log('Deploy Complete!');
